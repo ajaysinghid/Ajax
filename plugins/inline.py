5c9e7f83e574e46a89b851ee874a37d454f093cd
ajaysinghid <104294174+ajaysinghid@users.noreply.github.com>
2022-11-11 20:34:04 +0530

    Update inline.py

--- plugins/inline.py
+++ plugins/inline.py
@@ -93,7 +93,7 @@ def get_reply_markup(query):
         [
             InlineKeyboardButton('♻️ 𝗦𝗲𝗮𝗿𝗰𝗵 𝗔𝗴𝗮𝗶𝗻 ♻️', switch_inline_query_current_chat=query)
         ],[
-            InlineKeyboardButton('♥️ 𝗖𝗵𝗮𝗻𝗻𝗲𝗹 ♥️', url='https://t.me/MOVIEMASALA2_0')
+            InlineKeyboardButton('♥️ 𝗖𝗵𝗮𝗻𝗻𝗲𝗹 ♥️', url='https://t.me/MOVIEMASALA_official')
         ]
         ]
     return InlineKeyboardMarkup(buttons)
